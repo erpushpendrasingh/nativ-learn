2dc8fb0654b947bbb16d2d1db6b0ecb03637a439
Pushpendra Singh <90454357+erpushpendrasingh@users.noreply.github.com>
2024-04-28 23:43:48 +0530

    done

---
+++ AwesomeProject/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import {SafeAreaView, Text, View} from 'react-native';
+const App = () => {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>Hello world!!</Text>
+        <Text>Hello world!!</Text>
+        <Text>Hello world!!</Text>
+        <Text>Hello world!!</Text>
+        <Text>Hello world!!</Text>
+        <Text>Hello world!!</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default App;
